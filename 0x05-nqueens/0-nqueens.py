f04ad0f75d55c8369b7aed2c6f7129ccab0abafe
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-08-31 07:07:26 -0700

    The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard.

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,68 +1,53 @@
 #!/usr/bin/python3
-"""
-Solution to the nqueens problem
-"""
 import sys
 
+def nqueens(N):
+    def is_safe(board, row, col):
+        """Check if there is any queen in the same column"""
+        for i in range(row):
+            if board[i] == col:
+                return False
 
-def backtrack(r, n, cols, pos, neg, board):
-    """
-    backtrack function to find solution
-    """
-    if r == n:
-        res = []
-        for l in range(len(board)):
-            for k in range(len(board[l])):
-                if board[l][k] == 1:
-                    res.append([l, k])
-        print(res)
-        return
+        """Check if there is any queen in the same diagonal"""
+        for i in range(row):
+            if abs(board[i] - col) == abs(i - row):
+                return False
 
-    for c in range(n):
-        if c in cols or (r + c) in pos or (r - c) in neg:
-            continue
+        return True
 
-        cols.add(c)
-        pos.add(r + c)
-        neg.add(r - c)
-        board[r][c] = 1
+    def solve(board, row):
+        if row == N:
+            # Print the solution
+            print(','.join(str(col) for col in board))
+        else:
+            for col in range(N):
+                if is_safe(board, row, col):
+                    board[row] = col
+                    solve(board, row + 1)
 
-        backtrack(r+1, n, cols, pos, neg, board)
+    """Create an empty board"""
+    board = [-1] * N
 
-        cols.remove(c)
-        pos.remove(r + c)
-        neg.remove(r - c)
-        board[r][c] = 0
-
-
-def nqueens(n):
-    """
-    Solution to nqueens problem
-    Args:
-        n (int): number of queens. Must be >= 4
-    Return:
-        List of lists representing coordinates of each
-        queen for all possible solutions
-    """
-    cols = set()
-    pos_diag = set()
-    neg_diag = set()
-    board = [[0] * n for i in range(n)]
-
-    backtrack(0, n, cols, pos_diag, neg_diag, board)
+    """Start solving from the first row"""
+    solve(board, 0)
 
 
-if __name__ == "__main__":
-    n = sys.argv
-    if len(n) != 2:
+if __name__ == '__main__':
+    """Check the number of arguments"""
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+
     try:
-        nn = int(n[1])
-        if nn < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        nqueens(nn)
+        N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
+
+    """Check the value of N """
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    """Solve the N-queens problem"""
+    nqueens(N)i

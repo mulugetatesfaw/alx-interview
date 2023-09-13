ebffe6250d5b7be9307a9920c5fa254370c2c727
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-09-13 04:20:47 -0700

    rotate matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Rotate a square 2D matrix 90 degrees clockwise in-place.
+
+Args:
+    matrix (list): A square 2D matrix (list of lists).
+
+Returns:
+    None. The matrix is edited in-place.
+
+Raises:
+    None.
+"""
+
 def rotate_2d_matrix(matrix):
     """
     Rotate a square 2D matrix 90 degrees clockwise in-place.
@@ -21,3 +36,13 @@ def rotate_2d_matrix(matrix):
     # Reverse each row
     for i in range(n):
         matrix[i] = matrix[i][::-1]
+
+
+# Test the function
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)

374682eb5d4ac7104015082ad52a64e132b886f7
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-09-29 13:32:40 -0700

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,20 +1,56 @@
 #!/usr/bin/python3
+""" Island Perimeter mandatory """
+
+
+def test0(n):
+    """
+    *******************************
+    ******test if n is 0 or 1******
+    *******************************
+    @n: number of the grid
+    Returns: 0 if n is 1 or 1 if n is 0
+    """
+    if (n == 0):
+        return 1
+    return 0
+
+
 def island_perimeter(grid):
-    if not grid:
-        return 0
+    """
+    ***********************************************
+    ******returns the perimeter of the island******
+    ***************described in grid***************
+    ***********************************************
+    @grid: is a list of list of integers:
+                *** 0 represents water
+                *** 1 represents land
+    """
+    row = len(grid)
+    col = len(grid[0])
+    assert (1 <= row and col <= 100), "length must be between 1 an 100"
 
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
-
-    for i in range(rows):
-        for j in range(cols):
+    p = 0
+    for i in range(row):
+        for j in range(col):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                                        "grid numbers must be 0 or 1"
             if grid[i][j] == 1:
-                perimeter += 4  # Start with the assumption of a full perimeter
-                # Check neighbors and subtract the shared sides
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                if i-1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i-1][j])
+                if j-1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i][j-1])
 
-    return perimeter
+                try:
+                    p += test0(grid[i+1][j])
+                except IndexError:
+                    p += 1
+                try:
+                    p += test0(grid[i][j+1])
+                except IndexError:
+                    p += 1
+
+    return pi

518c34d95cb93b99f912b83b52e7316ac9aca48a
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-10-02 23:32:37 -0700

    the prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,26 +1,29 @@
+#!/usr/bin/python3
+"""Program that performs prime game"""
+
+
 def isWinner(x, nums):
+    """Function that performs prime game"""
     if not nums or x < 1:
         return None
-
-    def generate_primes(n):
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-
-        for i in range(2, int(pow(n, 0.5)) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-
-        return primes
-
-    prime_numbers = generate_primes(max(nums))
-    winner = None
-
+    n = max(nums)
+    fltr = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not fltr[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            fltr[j] = False
+    fltr[0] = fltr[1] = False
+    a = 0
+    for i in range(len(fltr)):
+        if fltr[i]:
+            a += 1
+        fltr[i] = a
+    plyr1 = 0
     for n in nums:
-        prime_count = sum(prime_numbers[:n + 1])
-        if prime_count % 2 == 0:
-            winner = 'Ben' if winner == 'Maria' else 'Maria'
-        else:
-            winner = 'Maria' if winner == 'Ben' else 'Ben'
-
-    return winner
+        plyr1 += fltr[n] % 2 == 1
+    if plyr1 * 2 == len(nums):
+        return None
+    if plyr1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"

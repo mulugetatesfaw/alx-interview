55936bb78ba2935f90c988f7342ba5f0ac696596
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-10-02 23:29:13 -0700

    the prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,28 +1,26 @@
-#!/usr/bin/python3
-def generate_primes(n):
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
-
-    p = 2
-    while p * p <= n:
-        if primes[p]:
-            for i in range(p * p, n + 1, p):
-                primes[i] = False
-        p += 1
-
-    return primes
-
 def isWinner(x, nums):
-    winner = None
+    if not nums or x < 1:
+        return None
+
+    def generate_primes(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+
+        for i in range(2, int(pow(n, 0.5)) + 1):
+            if primes[i]:
+                for j in range(i * i, n + 1, i):
+                    primes[j] = False
+
+        return primes
+
     prime_numbers = generate_primes(max(nums))
+    winner = None
 
     for n in nums:
         prime_count = sum(prime_numbers[:n + 1])
         if prime_count % 2 == 0:
-            # If the total count of prime numbers is even, Ben wins
             winner = 'Ben' if winner == 'Maria' else 'Maria'
         else:
-            # If the total count of prime numbers is odd, Maria wins
             winner = 'Maria' if winner == 'Ben' else 'Ben'
 
     return winner
